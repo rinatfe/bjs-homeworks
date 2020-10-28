d7c484e862a8580b9b7dc231082a6517f767905e
Rinat Kamaletdinov <sky2208@ya.ru>
2020-10-28 21:49:27 +0400

    done

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,29 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	// реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let total = (algebra + geography + physics)/3;
     
-	return // ваш ответ;
+	return total// ваш ответ;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
+	return  message // ваш ответ;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+    let result = (x * y) + (5 * z) + x - 1;
+    return  result// ваш ответ;
 }
