9a3242fdf7f6b6dcdd376344b42b998bf164d148
Rinat Kamaletdinov <sky2208@ya.ru>
2020-11-06 19:48:00 +0400

    task 1.3 done

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    for (let i in arguments) {
+        if (typeof arguments[i] !== 'number') {
+          arguments[i] = Number(arguments[i])
+          if (isNaN(Number(arguments[i]))) {
+            console.log('Невозможно преобразовть к числу')
+            break
+          } 
+        }
+      }
+      
+      let year1 = new Date(date).getFullYear();
+      let month1 = new Date(date).getMonth();
+      let year2 = new Date().getFullYear();
+      let month2 = new Date().getMonth();
+      let numberOfMonths = (year1 - year2) * 12 + (month1 - month2);
+      
+      
+      let bodyMortage = amount - contribution;
+      let precentItem = percent / 1200;
+      let payment = bodyMortage * (precentItem + precentItem / (((1 + precentItem) ** numberOfMonths ) - 1))
+      let totalAmount = (payment * numberOfMonths).toFixed(2);
+      console.log(totalAmount)
+      return Number(totalAmount);
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    if (arguments.length === 0 || name === "") {
+      return "Привет, мир! Меня зовут Аноним."
+    } else {
+      return `Привет, мир! Меня зовут ${name}.`
+    }
+    
 }
